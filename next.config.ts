41583883d6299d0c5f35aa4bc2a639f0e485d743
Humaidlol <humaidsadath2004@gmail.com>
2025-02-12 20:42:29 +0530

    made it a pwa

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,22 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig = withPWA({
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-};
+});
 
 export default nextConfig;
